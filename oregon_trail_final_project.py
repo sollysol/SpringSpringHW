0c2d7764b3a82e555bc3e5e308e911e3f1d53904
sollysol <35598370+sollysol@users.noreply.github.com>
2018-05-02 13:41:26 -0600

    Add files via upload

---
+++ oregon_trail_final_project.py
@@ -0,0 +1,66 @@
+#what is your name
+main_person = input("What is your name, young prospector?!")
+#what is the name of your travelling companion numero uno
+companion_uno =input("What is your fand's name?")
+#companion number 2
+companion_dos =input("Y, what is your second frand's name?")
+#companion number 3
+companion_tres= input(" ...and, the name of your third homie is?")
+#companion number 4
+companion_quatro = input("yai yai! let me get the name of your fourth homie-frand")
+
+date = input("Do you want to start your trip on the default start date of March 3 1847? For yes, enter Y. To use your own date enter N"
+
+if date = Y
+Y = 0303
+    #do whatever I have to do next
+else:
+    own_date = input("Enter the month and date you would like to start your journey. Your departure date must be between March 1 and May 1 ")
+    print(own_date)
+#default_start date
+
+
+print("YOU HAD SAVED $1600 TO SPEND FOR THE TRIP, AND YOU'VE JUST PAID $200 FOR A WAGON. YOU WILL NEED TO SPEND THE REST OF YOUR MONEY ON THE FOLLOWING ITEMS:"
+print("OXEN. Because America doesn't have water buffalos. THE MORE YOU SPEND, THE FASTER YOU'LL GO BECAUSE YOU'LL HAVE BETTER ANIMALS"
+print("FOOD. THE MORE YOU HAVE, THE LESS CHANCE THERE IS of you running out and before you know it, you're a cannibal and you have a grill named after you on CU Campus"
+print("AMMUNITION. Because let's be real. The purpose of your trip is to borrow some land that kinda doesn't belong to you. Oh, and you'll need ammo to hunt food."
+print(" You'll need MISCELLANEOUS SUPPLIES such as medicine lest you get ill, or for supplies wagon repairs"
+
+print("YOU CAN SPEND ALL YOUR MONEY BEFORE YOU START YOUR TRIP, OR YOU CAN SAVE SOME OF YOUR CASH TO SPEND AT FORTS ALONG THE WAY WHEN YOU RUN LOW. HOWEVER, ITEMS COST MORE AT THE FORTS."
+print("YOU CAN ALSO GO HUNTING ALONG THE WAY TO GET MORE FOOD")
+print("So, let me tell you about my capitalistic enterprise works:")
+
+Oxen:
+##The user has to spend between $100 and $200 dollars on oxen, inclusive.
+##There are 2 oxen in a yoke and the price of each yoke is $40. Present the oxen price information to the user and ask how many yokes do they wish to purchase.
+
+store_bill =float(ox_cost + food_cost + bullet_cost + misc_supplies)
+print("You must purchase $100 - $200 on oxen. There are 2 oxen in a yoke. Each yoke costs $40.")
+oxen = input("Enter the number of yokes of oxen you would like to purchase")
+ox_cost = (oxen * 40)
+    if ox_cost < 100 or ox_cost > 200
+        print("you can only spend $100 - $200 on oxen. Please choose again")
+        #want program to ask them again for number of oxen
+    return oxen
+
+else:
+    if ox_cost >= 100 and ox_cost <=200
+    print("the cost of your oxen is", ox_cost)
+print(store_bill)
+
+print("yall are going to need some food! As a profiteer, I recomend that you purchase at least 200lbs of comida per person. Food costs .50c per pound")
+food = input("how much food do you wanna purchase?")
+food_cost = float(food * .5)
+print(store_bill)
+
+print("Ahh. You're going to need lotsa bullets. You don't know it just yet but this is where our gun culture frontier state fun comes from. We'll still be acting like we're going on a frontier trip 100 years from now.")
+bullets = input("A box of 20 slugs costs $2. How many boxes would you like to purchase?")
+bullet_cost = (bullets * 2)
+print(store_bill)
+
+print("Miscellaneous Supplies are needed. You can purchase wagon parts or medical kits. Wagon parts cost $10 and medical kits cost $15")
+wagon_parts = input("Would you like to purchase some wagon parts? Enter the number of parts that you would like to purchase. Enter 0 if you don't want to buy any.")
+wagon_part_cost = (wagon_parts * 10)
+A medical aid kit costs $15.
+Ask the user how many kits they wish to purchase.
+Compute and print the current bill so far (oxen + food + bullets + misc supplies).'''
